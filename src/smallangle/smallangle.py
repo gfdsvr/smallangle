42fbf77dffa3a4f6ebece721f643d8f92a77fd24
César <cesar.faber@student.uva.nl>
2024-11-14 10:52:34 +0100

    smallangle
    beginning with adding clik option. still need to add command in pyproject.toml to give commando in the terminal

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,18 +3,23 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.command()
+@click.argument("number")
+@click.option(
+    "-n",
+    "--number",
+    default = 1,
+)
 
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
 
-
 if __name__ == "__main__":
-    sin(10)
+    sin()
